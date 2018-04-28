f58617bd5ab2c93f139f7a9125407722bdff82a0
yoh1 <yoh1@nd.edu>
2018-04-27 22:22:32 -0400

    Yejee: started GUI

--- trie_main.py
+++ trie_main.py
@@ -1,85 +1,87 @@
 #!/usr/bin/env python3
 
-from typing import Tuple
+'''
+Spring 2018 Data Structures Final Project
+Yejee Oh (yoh1), Olivia Wang (owang1)
+Trie Implementation
+'''
 
+# IMPORTS
 
-class TrieNode(object):
-    """
-    Our trie node implementation. Very basic. but does the job
-    """
-    
-    def __init__(self, char: str):
-        self.char = char
-        self.children = []
-        # Is it the last character of the word.`
-        self.word_finished = False
-        # How many times this character appeared in the addition process
-        self.counter = 1
-    
+import os
+import sys
+import string
 
-def add(root, word: str):
-    """
-    Adding a word in the trie structure
-    """
+# CREATE CLASS
+
+class Node():
+    def __init__(N, char: str):
+        N.value = char
+        N.end_word = False
+        N.children = set([])
+
+# INSERT FUNCTION
+
+def insert(root, word: str):
+
+    # Set node equal to root
     node = root
-    for char in word:
-        found_in_child = False
-        # Search for the character in the children of the present `node`
+
+    # Iterate through given word
+    for c in word:
+
+        # Keep track of whether or not character is found in children
+        in_children = False
+
+        # Set in_children to false if c is not in children nodes
         for child in node.children:
-            if child.char == char:
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-                child.counter += 1
-                # And point the node to the child that contains this char
+
+            # If character is in children, point node to child
+            if child.value == c:
+                in_children = True
                 node = child
-                found_in_child = True
-                break
-        # We did not find it so add a new chlid
-        if not found_in_child:
-            new_node = TrieNode(char)
-            node.children.append(new_node)
-            # And then point node to the new child
-            node = new_node
-    # Everything finished. Mark it as the end of a word.
-    node.word_finished = True
+                break;
 
+            # If character is NOT in children, add a new child
+            if not in_children:
+                next_node = Node(c)
+                node.children.append(next_node)
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
-    """
-    Check and return 
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
-    """
+    # Mark last character as leaf node
+    node.end_word = True
+
+# SEARCH FUNCTION
+
+def search(root, word: str):
+
+    # Set node equal to root
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
+
+    # Check if root has no children
     if not root.children:
-        return False, 0
-    for char in prefix:
-        char_not_found = True
-        # Search through all the children of the present `node`
+        return False
+
+    # Iterate through given word
+    for c in word:
+
+        # Keep track of whether or not character is found
+        char_found = False
+
         for child in node.children:
-            if child.char == char:
-                # We found the char existing in the child.
-                char_not_found = False
-                # Assign node as the child containing the char and break
-                node = child
-                break
-        # Return False anyway when we did not find a char.
-        if char_not_found:
-            return False, 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
-    return True, node.counter, 
+
+            # If character is found in existing child, set char_found to True
+            char_found = True
+            node = child
+            break
+
+        # If character is not found, return False
+        if not char_found:
+            return False
+
+    # If not return before, the word has been found; return True
+    return True
+
+# MAIN
 
 if __name__ == "__main__":
-    root = TrieNode('*')
-    add(root, "hackathon")
-    add(root, 'hack')
-
-    print(find_prefix(root, 'hac'))
-    print(find_prefix(root, 'hack'))
-    print(find_prefix(root, 'hackathon'))
-    print(find_prefix(root, 'ha'))
-    print(find_prefix(root, 'hammer'))
+    root = Node('*')
